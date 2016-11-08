7f65ea62e249cfc60ff6a3b173f7615e57fbcb75
aermakovucsb <aermakov@umail.ucsb.edu>
2016-11-08 21:42:18 +0000

    fixed issue #35

--- src/edu/ucsb/cs56/projects/games/hangman/HangmanGameTest.java
+++ src/edu/ucsb/cs56/projects/games/hangman/HangmanGameTest.java
@@ -8,100 +8,141 @@ import static org.junit.Assert.* ;
 // Test HangmanGame
 public class HangmanGameTest {
     @Test
-    public void testGameSessionWin() throws AlreadyTriedException {
-        HangmanGame hg = new HangmanGame("magic");
-        assertEquals("*****", hg.getBoard());
+    public void rightLengthOfUnknownWord()
+    {
+        HangmanGame hg = new HangmanGame("magics");
+        assertEquals("******", hg.getBoard());
         assertEquals(6, hg.getWrongAttemptsLeft());
         assertFalse(hg.hasWon());
         assertFalse(hg.hasLost());
-
+    }
+    @Test
+    public void singleLetterTest1() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
         hg.guessLetter('m');
         assertEquals("m****", hg.getBoard());
         assertEquals(6, hg.getWrongAttemptsLeft());
         assertFalse(hg.hasWon());
         assertFalse(hg.hasLost());
-
-        hg.guessLetter('a');
-        assertEquals("ma***", hg.getBoard());
+    }
+    @Test
+    public void singleLetterTest2() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
+        hg.guessLetter('g');
+        assertEquals("**g**", hg.getBoard());
         assertEquals(6, hg.getWrongAttemptsLeft());
         assertFalse(hg.hasWon());
         assertFalse(hg.hasLost());
-
-        hg.guessLetter('s');
-        assertEquals("ma***", hg.getBoard());
-        assertEquals(5, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-	boolean thrown = false;
-	try {
-	    hg.guessLetter('s');
-	} catch(AlreadyTriedException e) {
-	    thrown = true;
-	}
-	assertTrue(thrown);
-        assertEquals("ma***", hg.getBoard());
-        assertEquals(5, hg.getWrongAttemptsLeft()); // will not take out an attempt if already used letter
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
+    }
+    @Test
+    public void singleLetterTest3() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
         hg.guessLetter('c');
-        assertEquals("ma**c", hg.getBoard());
-        assertEquals(5, hg.getWrongAttemptsLeft());
+        assertEquals("****c", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
         assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('g');
-        assertEquals("mag*c", hg.getBoard());
-        assertEquals(5, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('i');
-        assertEquals("magic", hg.getBoard());
-        assertEquals(5, hg.getWrongAttemptsLeft());
-        assertTrue(hg.hasWon());
         assertFalse(hg.hasLost());
     }
-
     @Test
-    public void testGameSessionLose() throws AlreadyTriedException {
+    public void singleLetterTest4() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
+        hg.guessLetter('x');
+        assertEquals("*****", hg.getBoard());
+        assertEquals(5, hg.getWrongAttemptsLeft());
+        assertFalse(hg.hasWon());
+        assertFalse(hg.hasLost());
+    }
+    @Test
+    public void singleLetterDuplicateTest1() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
+        hg.guessLetter('m');
+        assertEquals("m****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        boolean thrown = false;
+	    try    
+        {
+	        hg.guessLetter('m');
+	    } catch(AlreadyTriedException e) 
+        {
+	        thrown = true;
+	    }
+	    assertTrue(thrown);
+        assertEquals("m****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        assertFalse(hg.hasWon());
+        assertFalse(hg.hasLost());
+    }
+    public void singleLetterDuplicateTest2() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
+        hg.guessLetter('x');
+        assertEquals("*****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        boolean thrown = false;
+	    try    
+        {
+	        hg.guessLetter('x');
+	    } catch(AlreadyTriedException e) 
+        {
+	        thrown = true;
+	    }
+	    assertTrue(thrown);
+        assertEquals("*****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        assertFalse(hg.hasWon());
+        assertFalse(hg.hasLost());
+    }
+    @Test   
+    public void completeWinningGame() throws AlreadyTriedException
+    {
         HangmanGame hg = new HangmanGame("magic");
         assertEquals("*****", hg.getBoard());
         assertEquals(6, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
+        hg.guessLetter('m');
+        assertEquals("m****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        hg.guessLetter('a');
+        assertEquals("ma***", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        hg.guessLetter('g');
+        assertEquals("mag**", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        hg.guessLetter('i');
+        assertEquals("magi*", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        hg.guessLetter('c');
+        assertEquals("magic", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        assertTrue(hg.hasWon());
         assertFalse(hg.hasLost());
-
-        hg.guessLetter('b');
+    }
+    @Test   
+    public void completeLosingGame() throws AlreadyTriedException
+    {
+        HangmanGame hg = new HangmanGame("magic");
+        assertEquals("*****", hg.getBoard());
+        assertEquals(6, hg.getWrongAttemptsLeft());
+        hg.guessLetter('x');
         assertEquals("*****", hg.getBoard());
         assertEquals(5, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('d');
+        hg.guessLetter('w');
         assertEquals("*****", hg.getBoard());
         assertEquals(4, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('e');
+        hg.guessLetter('q');
         assertEquals("*****", hg.getBoard());
         assertEquals(3, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('f');
+        hg.guessLetter('t');
+        assertEquals("*****", hg.getBoard());
         assertEquals(2, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('h');
+        hg.guessLetter('y');
         assertEquals("*****", hg.getBoard());
         assertEquals(1, hg.getWrongAttemptsLeft());
-        assertFalse(hg.hasWon());
-        assertFalse(hg.hasLost());
-
-        hg.guessLetter('j');
+        hg.guessLetter('u');
         assertEquals("*****", hg.getBoard());
         assertEquals(0, hg.getWrongAttemptsLeft());
         assertFalse(hg.hasWon());
